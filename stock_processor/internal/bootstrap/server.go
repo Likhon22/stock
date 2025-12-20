bd9ee7d173e71da7703ca5d54fa8d770228ae226
Likhon22 <likhonsarker793@gmail.com>
2025-12-20 15:46:57 +0600

    feat: Implement worker pool and message processing logic in ProcessorService

--- stock_processor/internal/bootstrap/server.go
+++ stock_processor/internal/bootstrap/server.go
@@ -1,5 +1,88 @@
 package bootstrap
 
-type Application struct{
-	
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"stock-processor/config"
+	infra "stock-processor/internal/kafka"
+	"stock-processor/internal/service"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Application struct {
+    service  service.ProcessorService
+    consumer *infra.KafkaConsumer
+    ctx      context.Context
+    cancel   context.CancelFunc
+}
+
+func NewApp() *Application {
+    ctx, cancel := context.WithCancel(context.Background())
+    consumer := infra.NewKafkaConsumer(
+        config.KafkaBroker,
+        config.KafkaTopic,
+        config.ConsumerGroup,
+    )
+    processorService := service.NewProcessorService();
+    
+    return &Application{
+        service:  processorService,
+        consumer: consumer,
+        ctx:      ctx,
+        cancel:   cancel,
+    }
+}
+
+func (a *Application) Run() error {
+    fmt.Println("Stock Processor started")
+    
+    defer a.consumer.Close()
+    defer a.cancel()
+    
+    // Create jobs channel
+    jobs := make(chan kafka.Message, config.WorkerCount*2)
+    
+    // Start worker pool
+    wg := a.service.StartWorkers(a.ctx, jobs, config.WorkerCount)
+    
+    // Setup signal handling
+    sigChan := make(chan os.Signal, 1)
+    signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+    
+    // Goroutine to handle signals
+    go func() {
+        sig := <-sigChan
+        fmt.Printf("\nReceived signal %v, shutting down...\n", sig)
+        a.cancel()
+    }()
+    
+    fmt.Println("Workers started, consuming messages...")
+    
+    // Main loop - BOOTSTRAP reads from Kafka!
+    for {
+        msg, err := a.consumer.ReadMessage(a.ctx) 
+        if err != nil {
+            if err == context.Canceled {
+                fmt.Println("Consumer stopped by signal")
+            } else {
+                fmt.Printf("Error reading: %v\n", err)
+            }
+            break
+        }
+        
+        jobs <- msg  
+    }
+    
+    // Graceful shutdown
+    close(jobs)
+    fmt.Println("Waiting for workers to finish...")
+    wg.Wait()
+    fmt.Println("Shutdown complete!")
+    
+    return nil
 }
--- stock_processor/internal/service/service.go
+++ stock_processor/internal/service/service.go
@@ -1,3 +1,66 @@
 package service
 
-// Worker pool and orchestration logic
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"stock-processor/internal/model"
+
+	"sync"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type ProcessorService interface {
+    StartWorkers(ctx context.Context, jobs <-chan kafka.Message, workerCount int) *sync.WaitGroup
+    ProcessMessage(ctx context.Context, msg kafka.Message) error
+}
+
+
+type processorService struct {
+
+}
+
+// Constructor - create new service
+func NewProcessorService() ProcessorService {
+    return &processorService{}
+}
+
+func (s *processorService) StartWorkers(ctx context.Context, jobs <-chan kafka.Message, workerCount int) *sync.WaitGroup  {
+	
+	var wg sync.WaitGroup
+  for w := 0; w < workerCount; w++ {
+        wg.Add(1)
+        go s.worker(ctx, w, jobs, &wg)
+    }
+	return &wg	
+}
+
+
+func (s *processorService) ProcessMessage(ctx context.Context, msg kafka.Message) error  {
+	  var stock model.StockPrice
+    
+    if err := json.Unmarshal(msg.Value, &stock); err != nil {
+        return fmt.Errorf("failed to parse message: %w", err)
+    }
+    
+    fmt.Printf("Received: %s at $%.2f at %s\n", 
+        stock.Symbol, 
+        stock.Price, 
+        stock.Timestamp.Format("15:04:05"))
+    
+    return nil
+
+}
+
+func (s *processorService) worker(ctx context.Context, id int, jobs <-chan kafka.Message, wg *sync.WaitGroup) {
+
+    defer wg.Done()
+		fmt.Printf("Worker %d started\n", id)
+  for msg:= range jobs {
+		 if err := s.ProcessMessage(ctx, msg); err != nil {
+            fmt.Printf("Worker %d error: %v\n", id, err)
+    }
+	}
+    fmt.Printf("Worker %d stopped\n", id)
+}
