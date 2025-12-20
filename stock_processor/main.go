10ce71e93b806c5477c85c574b74a6d190f1632b
Likhon22 <likhonsarker793@gmail.com>
2025-12-20 15:48:27 +0600

    refactor: Simplify main function by integrating application bootstrap and removing unused Kafka consumer logic

--- stock_processor/main.go
+++ stock_processor/main.go
@@ -1,71 +1,14 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"os"
-	"os/signal"
-	"stock-processor/config"
-	infra "stock-processor/internal/kafka"
-	"stock-processor/internal/model"
-	"sync"
-	"syscall"
-
-	"github.com/segmentio/kafka-go"
+	"log"
+	"stock-processor/internal/bootstrap"
 )
 
 func main() {
-    fmt.Println("Stock Processor started")
-    var wg sync.WaitGroup
-    // Create consumer
-    consumer := infra.NewKafkaConsumer(
-        config.KafkaBroker,
-        config.KafkaTopic,
-        config.ConsumerGroup,
-    )
-    defer consumer.Close()  // Don't forget to close!
-    
-ctx, cancel := context.WithCancel(context.Background())
-defer cancel()
-sigChan:=make(chan os.Signal, 1)
-signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-go func ()  {
-	  sig := <-sigChan
-    fmt.Printf("\nReceived signal %v, shutting down...\n", sig)
-    cancel()
-}()
-    jobs:=make(chan kafka.Message, len(config.Symbols)*2)
-  
-		 for i := 0; i < config.WorkerCount; i++ {
-			wg.Add(1)
-			go worker(jobs,&wg)
-		 }
-
-		 for{
-
-			msg,err:=consumer.ReadMessage(ctx)
-			  if err != nil {
-        break
-    }
-	   	jobs<-msg
-		 }
-   
-  close(jobs)		 
-	wg.Wait()	
-}
-
-
-func worker(jobs <-chan kafka.Message,wg *sync.WaitGroup)  {
-	defer wg.Done()
-	for msg := range jobs {
-		var stock model.StockPrice
-            if err := json.Unmarshal(msg.Value, &stock); err != nil {
-                fmt.Printf("Error parsing: %v\n", err)
-               continue 
-            }
-            
-						   fmt.Printf("Received: %s at $%.2f\n", stock.Symbol, stock.Price)
+ 	app := bootstrap.NewApp()
+	if err := app.Run(); err != nil {
+		log.Fatalf("Application failed: %v", err)
 	}
 	
 }
